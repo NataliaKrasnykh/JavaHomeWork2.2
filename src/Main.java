fb8bd5732dd645c590f0e17997c80ad8e2773239
Natalia Krasnykh <krasnih.n161@mail.ru>
2024-09-04 12:39:40 +0300

    Bonus

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
+        int InitialBalance = 150;
+        int payment = 1050;
+        int bonus;
+        int balance;
 
+        if (payment > 1000) {
+            bonus = payment / 100;
+
+            balance = InitialBalance + payment + bonus;
+
+            System.out.println("Начислено " + bonus + " рублей бонусов. Итоговый баланс " + balance + " руб.");
+        } else {
+            balance = InitialBalance + payment;
+
+            System.out.println("Бонусы к начислению отсутствуют. Итоговый баланс " + balance + " руб.");
+        }
     }
 }
